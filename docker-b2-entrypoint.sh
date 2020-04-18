f6358baedcef336133f53d95dd01add483471a78
Kevin Lang <me@gnalck.com>
2020-04-18 13:27:13 -0700

    fix issue with over-nesting

--- docker-b2-entrypoint.sh
+++ docker-b2-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf "$GHOST_CONTENT/adapters/storage/b2/"
-mkdir -p "$GHOST_CONTENT/adapters/storage/b2/"
-cp -r "$GHOST_INSTALL/content.orig/adapters/storage/b2/" "$GHOST_CONTENT/adapters/storage/b2/"
+mkdir -p "$GHOST_CONTENT/adapters/storage/"
+cp -r "$GHOST_INSTALL/content.orig/adapters/storage/b2/" "$GHOST_CONTENT/adapters/storage/"
 
 exec docker-entrypoint.sh "$@"
